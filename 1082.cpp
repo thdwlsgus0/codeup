0e9ce18f9c4851988f502575a9eee37021568705
thdwlsgus0 <thdwlsgus0@naver.com>
2019-07-04 22:05:53 +0900

    Create 1082.cpp

---
+++ 1082.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	int a;
+	scanf("%X", &a);
+	for (int i = 1; i <= 15; i++)
+	{
+		printf("%X*%X=%X\n", a, i, a * i);
+	}
+	return 0;
+}
