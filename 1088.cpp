a7f1735232ded4aee2d2613eb004f3b75d87c6e4
thdwlsgus0 <thdwlsgus0@naver.com>
2019-07-04 22:06:22 +0900

    Create 1088.cpp

---
+++ 1088.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0)continue;
+		else printf("%d ", i);
+	}
+	return 0;
+}
