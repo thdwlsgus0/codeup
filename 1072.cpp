3988e050d1bb1e259220892ec0444998985351af
thdwlsgus0 <thdwlsgus0@naver.com>
2019-07-04 22:03:22 +0900

    Create 1072.cpp

---
+++ 1072.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	int n,data;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &data);
+		printf("%d\n", data);
+	}
+	return 0;
+}
