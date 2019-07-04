e7e5404e0dbcc573366b1f6ad1272a8322b6d37d
thdwlsgus0 <thdwlsgus0@naver.com>
2019-07-04 22:03:21 +0900

    Create 1071.cpp

---
+++ 1071.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	int data;
+	while (1)
+	{
+		scanf("%d", &data);
+		if (data == 0)
+		{
+			printf("%d\n", data);
+			break;
+		}
+		printf("%d\n", data);
+	}
+	return 0;
+
+}
