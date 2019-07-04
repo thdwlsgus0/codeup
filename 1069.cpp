a9ef743f2180cb43851faff409f64fadbab68ce6
thdwlsgus0 <thdwlsgus0@naver.com>
2019-07-04 22:02:47 +0900

    Create 1069.cpp

---
+++ 1069.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+	char a;
+	scanf("%c", &a);
+	if (a == 'A')printf("best!!!");
+	else if (a == 'B')printf("good!!");
+	else if (a == 'C')printf("run!");
+	else if (a == 'D')printf("slowly~");
+	else printf("what?");
+	return 0;
+}
